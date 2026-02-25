82b85dbf90e67641bceb2ccbf2cb722f23f4c5c3
eighh1 <35957748+zx3777@users.noreply.github.com>
2026-02-25 14:06:54 +0800

    Update drawList.js

--- popup/drawList.js
+++ popup/drawList.js
@@ -1,8 +1,20 @@
-var userInputs={};
+window.userInputs = window.userInputs || {};
 
-// 使用 window 对象明确获取主脚本中的全局变量
-document.getElementById('psshButton').addEventListener("click", () => drawList(window.psshs, 'pssh'));
-document.getElementById('licenseButton').addEventListener("click", () => drawList(window.requests.map(r => r['url']), 'license'));
+document.getElementById('psshButton').addEventListener("click", () => {
+    if (!window.psshs || window.psshs.length === 0) {
+        alert("暂未抓取到 PSSH 数据");
+        return;
+    }
+    drawList(window.psshs, 'pssh');
+});
+
+document.getElementById('licenseButton').addEventListener("click", () => {
+    if (!window.requests || window.requests.length === 0) {
+        alert("暂未抓取到 License 数据");
+        return;
+    }
+    drawList(window.requests.map(r => r['url']), 'license');
+});
 
 function writeListElement(items, outputVar, searchStr) {
     document.getElementById("items").innerHTML = '';
@@ -18,22 +30,22 @@ function writeListElement(items, outputVar, searchStr) {
 }
 
 function drawList(items, outputVar) {
-    document.getElementById('home').style.display='none';
-    document.getElementById('chooserContainer').style.display='grid';
-    document.getElementById('toggleHistory').style.display='none';
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('chooserContainer').style.display = 'grid';
+    document.getElementById('toggleHistory').style.display = 'none';
 
-    writeListElement(items, outputVar, null)
+    writeListElement(items, outputVar, null);
     document.getElementById("chooserSearch").addEventListener('input', event => {
         const searchStr = event.target.value.toLowerCase();
-        writeListElement(items, outputVar, searchStr)
+        writeListElement(items, outputVar, searchStr);
     });
 }
 
 function itemSelected(index, item, outputVar){
-    userInputs[outputVar]=index;
-    document.getElementById(outputVar).value=item;
-    document.getElementById('chooserContainer').style.display='none';
-    document.getElementById('home').style.display='grid';
-    document.getElementById('toggleHistory').style.display='grid';
-    document.getElementById("chooserSearch").value=""
+    window.userInputs[outputVar] = index;
+    document.getElementById(outputVar).value = item;
+    document.getElementById('chooserContainer').style.display = 'none';
+    document.getElementById('home').style.display = 'grid';
+    document.getElementById('toggleHistory').style.display = 'grid';
+    document.getElementById("chooserSearch").value = "";
 }
